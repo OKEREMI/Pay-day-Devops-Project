d1998a31efedcf835445cc6a86209e570857a4a2
TripleAze <aliyuatikuabubakar7@gmail.com>
2026-02-19 16:58:19 +0100

    chore: add _CI_TRIGGER_3 constant for CI trigger.

--- source_code/auth-service/src/index.ts
+++ source_code/auth-service/src/index.ts
@@ -20,6 +20,7 @@ const JWT_SECRET = process.env.JWT_SECRET || 'payday_super_secret_key';
 const AUTH_URL = process.env.AUTH_URL || `http://localhost:4001`;
 const PAYMENT_URL = process.env.PAYMENT_URL || 'http://localhost:4002';
 const _CI_TRIGGER_2 = true; // ci-trigger: remove this line after pipeline is okay
+const _CI_TRIGGER_3 = true; // ci-trigger: remove this line after pipeline is okay
 
 const swaggerOptions = {
     definition: {
